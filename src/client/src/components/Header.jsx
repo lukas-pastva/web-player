96d7ca66513d99680eae44a4adc0d311ae7ec511
Lukas Pastva <lukas.pastva@o2.sk>
2025-06-06 22:53:27 +0200

    .

--- src/client/src/components/Header.jsx
+++ src/client/src/components/Header.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 
 export default function Header() {
-  const p      = window.location.pathname;
   const domain = (window.location.hostname || "Web-Player").replace(/^www\./, "");
 
   return (
     <header className="mod-header">
       <h1>{domain}</h1>
-
-      <nav className="nav-center">
-        <a href="/media" className={p.startsWith("/media") ? "active" : ""}>
-          Library
-        </a>
-      </nav>
     </header>
   );
 }
